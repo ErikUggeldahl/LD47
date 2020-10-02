c70fb1125c5569df911aae936e25e7ec0d660d52
Erik Uggeldahl <erikuggeldahl@gmail.com>
2020-10-02 16:58:01 -0700

    Basic player and camera control

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    readonly Vector3 OFFSET = new Vector3(-5f, 7f, -5f);
+    const float FOLLOW_FACTOR = 5f;
+
+    void Update()
+    {
+        var idealPosition = target.position + OFFSET;
+        transform.position = Vector3.Lerp(transform.position, idealPosition, FOLLOW_FACTOR * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(PlayerInput))]
+public class CharacterControl : MonoBehaviour
+{
+    [SerializeField] CharacterController controller;
+    [SerializeField] PlayerInput input;
+
+    const float SPEED = 5f;
+    readonly Vector3 GRAVITY = Physics.gravity;
+    readonly Quaternion ISOMETRIC_ROTATION = Quaternion.AngleAxis(45f, Vector3.up);
+
+    InputAction moveAction;
+    
+
+    void Start()
+    {
+        moveAction = input.actions["move"];
+    }
+
+    void Update()
+    {
+        var velocity = new Vector3(0f, GRAVITY.y, 0f);
+
+        var moveInput = moveAction.ReadValue<Vector2>();
+        var moveVelocity = ISOMETRIC_ROTATION * new Vector3(moveInput.x, 0f, moveInput.y).normalized * SPEED;
+        velocity += moveVelocity;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
