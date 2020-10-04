0d85e2b87244147d068981306d95d5e286420e8d
Erik Uggeldahl <erikuggeldahl@gmail.com>
2020-10-04 02:52:56 -0700

    Added patrolling

--- Assets/Scripts/Guard.cs
+++ Assets/Scripts/Guard.cs
@@ -1,24 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Guard : MonoBehaviour
 {
     [SerializeField] GuardCap cap = null;
+    [SerializeField] NavMeshAgent agent = null;
+    [SerializeField] Transform patrolPath = null;
+
+    const float PATROL_ARRIVED_DISTANCE = 0.5f;
+    Transform[] patrolPoints;
+    int currentPatrolPoint = 0;
 
     void Start()
     {
-        
+        agent.autoBraking = false;
+
+        patrolPoints = patrolPath.GetComponentsInChildren<Transform>().Where(t => t != patrolPath).ToArray();
+        PatrolToCurrentPoint();
     }
 
     void Update()
     {
-        
+        if (!agent.pathPending && agent.remainingDistance < PATROL_ARRIVED_DISTANCE)
+        {
+            currentPatrolPoint = (currentPatrolPoint + 1) % patrolPoints.Length;
+            PatrolToCurrentPoint();
+        }
+    }
+
+    void PatrolToCurrentPoint()
+    {
+        agent.destination = patrolPoints[currentPatrolPoint].position;
     }
 
     public void Die()
     {
         cap.SetState(GuardCap.State.Dead);
+
+        agent.enabled = false;
     }
 
     public void Explode()
